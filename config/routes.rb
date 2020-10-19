e6058b6481410c43911c0c30dff8bff869bb2664
guillermo varela <guillovarela16@gmail.com>
2020-10-19 14:49:03 -0300

    Add model contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'contact/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
+  resources :contacts, only: [:new, :create]
 end
