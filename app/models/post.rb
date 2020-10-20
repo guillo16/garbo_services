0a34d0d8ac69cc2a0bc1f6258507e0b10fd537eb
guillermo varela <guillovarela16@gmail.com>
2020-10-20 20:06:26 -0300

    Add cloudinary

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
+  has_one_attached :photo
+
   validates :title, presence: true
   validates :subtitle, presence: true
+  validate :validate_photo
+
+  def validate_photo
+    return if photo.attached?
+
+    errors.add(:photo, "no puede estar vacio")
+  end
 end
